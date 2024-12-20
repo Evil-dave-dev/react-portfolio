db278407f2ff3c981c12ab833bd712baf9cdc9d2
david <evil.dave59@gmail.com>
2024-12-20 13:18:08 +0100

    loading google fonts in HTML

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,10 @@ function App({ Component, pageProps }) {
     <>
       <Head>
         <link rel="icon" href="/logo.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Anton&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=PT+Mono&display=swap"
+          rel="stylesheet"
+        />
         <title>Stevenoot David</title>
       </Head>
       <Component {...pageProps} />
